230f1a33dc1743957d59f3a62d92c9b8be655ecb
Dmitry Sergeev <dmitriysergeev101096@gmail.com>
2024-07-21 12:59:09 +0200

    basic config

---
+++ init.lua
@@ -0,0 +1,22 @@
+--Basic
+require('core.plugins')
+require('core.mappings')
+require("core.colors")
+require("core.configs")
+
+-- Plugins config
+require("plugins.telescope")
+require("plugins.lsp")
+require("plugins.mason")
+require("plugins.cmp")
+--require("plugins.nullls")
+--require("plugins.lualine")
+--require("plugins.gitsigns")
+require("plugins.treesitter")
+--require("plugins.toggleterm")
+--require("plugins.outline")
+--require("plugins.autopairs")
+--require("plugins.comments")
+--require("plugins.autotag")
+--require("plugins.buffline")
+require("plugins.neotree")
---
+++ lua/core/plugins.lua
@@ -0,0 +1,44 @@
+-- Lazy as Plugin Manager
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+        lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+
+require("lazy").setup({
+	{ "phaazon/hop.nvim" }, 
+	{
+	    "nvim-neo-tree/neo-tree.nvim",
+	    branch = "v3.x",
+	    dependencies = {
+	      "nvim-lua/plenary.nvim",
+	      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	      "MunifTanjim/nui.nvim",
+		}
+	}, 
+	{ "nvim-treesitter/nvim-treesitter" }, -- for easy install parsers
+	{ "neovim/nvim-lspconfig" }, 
+	-- colorschemes
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true},
+	--{"joshdick/onedark.vim"},
+	--{"rebelot/kanagawa.nvim"}, 
+	-- autocomplete
+	{"hrsh7th/cmp-nvim-lsp"}, 
+	{"hrsh7th/cmp-buffer"}, 
+	{"hrsh7th/cmp-path"},
+    	{"hrsh7th/cmp-cmdline"}, 
+	{"hrsh7th/nvim-cmp"}, 
+	{"lewis6991/gitsigns.nvim"},
+	-- mason for lsp servers
+	{"williamboman/mason.nvim", build = ":MasonUpdate"}, 
+	-- telescope for fast search
+	{"nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" }}
+
+})
